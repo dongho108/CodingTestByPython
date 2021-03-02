f131267a6c5402a8ed3b285ad060a942f238a3c1
dongho <dongho108@naver.com>
2021-03-02 14:39:47 +0900

    add solve sw maestro12 p5

--- SWmaestro12/5.py
+++ SWmaestro12/5.py
@@ -1,9 +1,11 @@
+from collections import deque
+
 def main():
     n = int(input())
 
     data = []
     max_time = 0
-    for i in range(n):
+    for i in range(n*n):
         data.append(list(map(int, input().split())))
         for j in range(2, len(data[i])):
             if max_time < data[i][j]:
@@ -13,12 +15,24 @@ def main():
 
     for i in range(len(data)):
         for j in range(2, len(data[i])):
-            print(data[i][j], data[i][0])
             time_graph[data[i][j]].append(data[i][0])
 
-    print(time_graph)
+    result = 0
+    for i in range(1, len(time_graph)):
+        result += max(time_graph[i])
+
+    print(result)
 
 
 
 if __name__ == "__main__":
     main()
+
+
+'''
+2
+1 3 1 3 5
+2 2 2 4
+3 2 1 2
+4 1 3
+'''
