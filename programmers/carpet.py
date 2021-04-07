650b11e2e6988717c90b9fe6044808d06b7efbd8
dongho <dongho108@naver.com>
2021-04-07 10:57:15 +0900

    add programmers problem

---
+++ programmers/carpet.py
@@ -0,0 +1,18 @@
+def solution(brown, yellow):
+    answer = []
+
+    for i in range(1, yellow+1):
+        if yellow % i == 0:
+            x = i
+            y = yellow // i
+
+            tbrown = (x+2)*(y+2) - yellow
+            if tbrown == brown:
+                answer.append(y+2)
+                answer.append(x+2)
+                break
+
+    return answer
+
+print(solution(24, 24))
+print(solution(8, 1))
