9a62425f8d5f85af83bdca009297e3e470cfdbbf
dongho <dongho108@naver.com>
2021-04-07 10:31:23 +0900

    update display space -> tab

--- my_library/display.py
+++ my_library/display.py
@@ -1,6 +1,6 @@
 def display(arr):
     for i in range(len(arr)):
         for j in range(len(arr)):
-            print(arr[i][j], end=' ')
+            print(arr[i][j], end='\t')
         print()
     print("=====")
